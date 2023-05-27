79ad403bc3b630d411e5008b1ac63d1e688aaaa0
SigalGinsburg <sginsburg@campus.technion.ac.il>
2023-05-27 17:10:10 +0300

    we finished the first part

--- src/Date.java
+++ src/Date.java
@@ -24,6 +24,30 @@ public class Date {
 
     }
 
+    public void setDay(int day) {
+        this.day = day;
+    }
+
+    public int getDay() {
+        return day;
+    }
+
+    public void setMonth(int month) {
+        this.month = month;
+    }
+
+    public int getMonth() {
+        return month;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
     protected boolean isDate() {
         return true;
     }
