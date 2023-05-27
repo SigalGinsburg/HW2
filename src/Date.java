ff54777b40e6e28cd349e2b0cfd5e6cc0ebd207a
SigalGinsburg <sginsburg@campus.technion.ac.il>
2023-05-27 15:34:33 +0300

    first draft

--- src/Date.java
+++ src/Date.java
@@ -1,2 +1,29 @@
 public class Date {
+    int year;
+    int month;
+    int day;
+public Date(int year, int month, int day) {
+    if (year >= -3999 && year <= 3999){
+        this.year = year;
+    }
+    else {
+        this.year = 0;
+    }
+    if (month >= 1 && month <= 12){
+        this.month = month;
+    }
+    else {
+        this.month = 1;
+    }
+
+    if (day >= 1 && day <= 31){
+        this.day = day;;
+    }
+    else {
+        this.day = 1;
+    }
+
+}
+
+
 }
