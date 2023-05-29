791091d3fd90ebab5461b32e1836e941bcf8518d
SigalGinsburg <sginsburg@campus.technion.ac.il>
2023-05-29 14:56:43 +0300

    we finished the first part

--- src/MultiSum.java
+++ src/MultiSum.java
@@ -1,2 +1,12 @@
-public class MultiSum {
+public class MultiSum extends Function {
+
+    private Function[] multiSumFunctionList;
+
+    public MultiSum(Integer[] functions) {
+    }
+
+    @Override
+    public Iterator<Integer> iterator() {
+        return new MultiSumIterator();
+    }
 }
--- src/Polynomial.java
+++ src/Polynomial.java
@@ -1,3 +1,15 @@
-public class Polynomial extends Function{
+public class Polynomial<T extends Number> extends Function {
+    private T[] polynom;
 
+    public Polynomial(T... nums) {
+        int listLength = nums.length;
+        polynom = (T[]) new Number[listLength];
+        int i = 0;
+        for (T num : nums) {
+            polynom[i] = num;
+            i++;
+        }
+    }
+
+    // Rest of the class implementation
 }
