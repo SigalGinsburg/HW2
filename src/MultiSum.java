13781cb09214f3fb3c60d3bbdd8ecdd60cee394e
mayasokolov <mayasokolov@campus.technion.ac.il>
2023-05-29 15:54:42 +0300

    we learned how to pronounce quotient and derivative <3

--- src/MultiSum.java
+++ src/MultiSum.java
@@ -1,6 +1,7 @@
 public class MultiSum extends Function {
 
-    private Function[] multiSumFunctionList;
+    protected Function[] multiSumFunctionList;
+    private int listLength;
 
     public MultiSum(Function... functions) {
         int listLength = functions.length;
@@ -10,6 +11,30 @@ public class MultiSum extends Function {
             multiSumFunctionList[i] = f;
             i++;
         }
+        this.listLength= listLength;
     }
+    @Override
+    public double valueAt(double point){
+        int resultValueAtMultiSum=0;
+        for (int i=0;i<listLength ;i++){
+            resultValueAtMultiSum+=multiSumFunctionList[i].valueAt(point);
+        }
+        return resultValueAtMultiSum;
+    }
+    @Override
+    public MultiSum derivative(){
+        Function[] functions=new Function[listLength];
+        for (int i=0;i<listLength ;i++){
+        functions[i]= multiSumFunctionList[i].derivative();
+        }
+        MultiSum derivativeMultiSum= new MultiSum(functions);
+        return derivativeMultiSum;
+    }
+
+
+
+
+
 }
+
 
